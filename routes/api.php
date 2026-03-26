623e97a818df373c0c3f696d844b79ae399f65e2
trantruong2203 <newhorizon6789@gmai.com>
2026-03-26 11:20:39 +0700

    refactor: Update MiniTournamentPaymentController to use participantId instead of paymentId for payment operations, enhancing clarity and consistency in API routes

--- routes/api.php
+++ routes/api.php
@@ -553,9 +553,9 @@ Route::middleware(['auth:api', 'update.last_login', 'throttle:api'])->group(func
         Route::get('/{id}/payments', [MiniTournamentPaymentController::class, 'index']);
         Route::post('/{id}/pay', [MiniTournamentPaymentController::class, 'pay']);
         Route::match(['get', 'post'], '/{id}/my-payment', [MiniTournamentPaymentController::class, 'myPayment']);
-        Route::post('/{id}/payments/{paymentId}/mark-paid', [MiniTournamentPaymentController::class, 'markPaid']);
-        Route::post('/{id}/payments/{paymentId}/confirm', [MiniTournamentPaymentController::class, 'confirm']);
-        Route::post('/{id}/payments/{paymentId}/reject', [MiniTournamentPaymentController::class, 'reject']);
+        Route::post('/{id}/payments/{participantId}/mark-paid', [MiniTournamentPaymentController::class, 'markPaid']);
+        Route::post('/{id}/payments/{participantId}/confirm', [MiniTournamentPaymentController::class, 'confirm']);
+        Route::post('/{id}/payments/{participantId}/reject', [MiniTournamentPaymentController::class, 'reject']);
         Route::post('/{id}/payments/remind/{participantId}', [MiniTournamentPaymentController::class, 'remind']);
         Route::post('/{id}/payments/remind-all', [MiniTournamentPaymentController::class, 'remindAll']);
 
