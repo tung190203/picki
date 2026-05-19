2218e862f3009c4636185a6e3baf6125f9b1e11b
trantruong2203 <newhorizon6789@gmail.com>
2026-05-19 19:15:34 +0700

    refactor: update notification type handling in ClubTournamentController, MiniTournamentService, and ClubActivityService to use enum values

--- database/seeders/ClubNotificationTypeSeeder.php
+++ database/seeders/ClubNotificationTypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\ClubNotificationType as EnumType;
 use App\Models\Club\ClubNotificationType;
 use Illuminate\Database\Seeder;
 
@@ -10,17 +11,23 @@ class ClubNotificationTypeSeeder extends Seeder
     public function run(): void
     {
         $types = [
-            ['name' => 'Thông báo chung', 'slug' => 'general', 'description' => 'Thông báo chung của CLB', 'icon' => 'bell', 'is_active' => true],
-            ['name' => 'Sự kiện', 'slug' => 'event', 'description' => 'Thông báo sự kiện, lịch tập', 'icon' => 'calendar', 'is_active' => true],
-            ['name' => 'Tài chính', 'slug' => 'finance', 'description' => 'Thu chi, đóng phí', 'icon' => 'currency', 'is_active' => true],
-            ['name' => 'Thành viên', 'slug' => 'member', 'description' => 'Thành viên mới, thay đổi vai trò', 'icon' => 'users', 'is_active' => true],
-            ['name' => 'Khẩn cấp', 'slug' => 'urgent', 'description' => 'Thông báo khẩn cấp', 'icon' => 'alert', 'is_active' => true],
+            ['enum' => EnumType::General, 'name' => 'Thông báo chung', 'slug' => 'general', 'description' => 'Thông báo chung của CLB', 'icon' => 'bell', 'is_active' => true],
+            ['enum' => EnumType::Event, 'name' => 'Sự kiện', 'slug' => 'event', 'description' => 'Thông báo sự kiện, lịch tập', 'icon' => 'calendar', 'is_active' => true],
+            ['enum' => EnumType::Finance, 'name' => 'Tài chính', 'slug' => 'finance', 'description' => 'Thu chi, đóng phí', 'icon' => 'currency', 'is_active' => true],
+            ['enum' => EnumType::Member, 'name' => 'Thành viên', 'slug' => 'member', 'description' => 'Thành viên mới, thay đổi vai trò', 'icon' => 'users', 'is_active' => true],
+            ['enum' => EnumType::Urgent, 'name' => 'Khẩn cấp', 'slug' => 'urgent', 'description' => 'Thông báo khẩn cấp', 'icon' => 'alert', 'is_active' => true],
         ];
 
         foreach ($types as $type) {
-            ClubNotificationType::firstOrCreate(
+            ClubNotificationType::updateOrCreate(
                 ['slug' => $type['slug']],
-                $type
+                [
+                    'id' => $type['enum']->value,
+                    'name' => $type['name'],
+                    'description' => $type['description'],
+                    'icon' => $type['icon'],
+                    'is_active' => $type['is_active'],
+                ]
             );
         }
     }
