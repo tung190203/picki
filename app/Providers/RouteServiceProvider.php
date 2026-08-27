243882fa598ad3dee9ff9d335aa721dba192a3cf
Nguyen Thanh Tung <tung19022003@gmail.com>
2026-08-27 10:33:11 +0700

    cloudflare setup and speed rate limit for unauth api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,6 +34,10 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
+        RateLimiter::for('auth-strict', function (Request $request) {
+            return Limit::perMinute(5)->by($request->ip());
+        });
+
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
